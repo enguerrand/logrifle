241635c5e93df4e8e486aacdcbcb8b7079aba57e
edr <github@rochefort.de>
2019-11-29 21:49:08 +0100

    Wait for Tailer completion in test

--- src/test/java/de/rochefort/logrifle/LogReaderTest.java
+++ src/test/java/de/rochefort/logrifle/LogReaderTest.java
@@ -2,7 +2,7 @@ package de.rochefort.logrifle;
 
 import de.rochefort.logrifle.data.Line;
 import de.rochefort.logrifle.data.LineParserTextImpl;
-import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,46 +13,53 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
+import java.util.function.BooleanSupplier;
 
 class LogReaderTest {
-    Path logfile = Paths.get("./out/log.log");
+    static Path LOGFILE = Paths.get("./out/log.log");
 
     @BeforeEach
     void setUp() throws IOException {
-        Files.deleteIfExists(logfile);
+        Files.deleteIfExists(LOGFILE);
     }
 
-    @AfterEach
-    void tearDown() throws IOException {
-//        Files.deleteIfExists(logfile);
+    @AfterAll
+    static void tearDown() throws IOException {
+        Files.deleteIfExists(LOGFILE);
     }
 
     @Test
-    void testReadStaticLog() throws IOException {
+    void testReadStaticLog() throws Exception {
         TestLogWriter logWriter = new TestLogWriter(null, 0L);
         logWriter.writeRandomLogLine();
         logWriter.writeRandomLogLine();
         logWriter.writeRandomLogLine();
-        LogReader logReader = new LogReader(new LineParserTextImpl(), logfile);
+        LogReader logReader = new LogReader(new LineParserTextImpl(), LOGFILE);
         List<Line> lines = logReader.getLines();
         Assertions.assertEquals(3, lines.size(), "wrong line count");
         Assertions.assertTrue(lines.get(1).getRaw().endsWith("ullam doloremque quia dolorem pariatur. adipiscing 0076.32 nesciunt. dolore"), "Wrong line content");
     }
 
     @Test
-    void testOpenDynamicLog() throws IOException, InterruptedException, ExecutionException {
+    void testOpenDynamicLog() throws Exception {
         TestLogWriter logWriter = new TestLogWriter(null, 0L);
         logWriter.writeRandomLines(100);
         CompletableFuture<Void> f = logWriter.start(100, 200);
-        LogReader logReader = new LogReader(new LineParserTextImpl(), logfile);
+        LogReader logReader = new LogReader(new LineParserTextImpl(), LOGFILE);
         f.get();
         List<Line> lines = logReader.getLines();
-
+        await(() -> lines.size() == 300, 10_000L);
         Assertions.assertEquals(300, lines.size(), "wrong line count");
         Assertions.assertTrue(lines.get(1).getRaw().endsWith("ullam doloremque quia dolorem pariatur. adipiscing 0076.32 nesciunt. dolore"), "Wrong line content");
         Assertions.assertTrue(lines.get(99).getRaw().endsWith("laboriosam, autem minima ut est ad qui veritatis sunt dolore in sit quae labore öäßaweawe( consequat."), "Wrong line content");
         Assertions.assertTrue(lines.get(100).getRaw().endsWith("aperiam, pariatur. veniam,"), "Wrong line content");
         Assertions.assertTrue(lines.get(299).getRaw().endsWith("eum qui nostrud ut"), "Wrong line content");
+    }
+
+    private void await(BooleanSupplier condition, long timeoutMs) throws InterruptedException {
+        long deadline = System.currentTimeMillis() + timeoutMs;
+        while (System.currentTimeMillis() < deadline && !condition.getAsBoolean()) {
+            Thread.sleep(10L);
+        }
     }
 }
